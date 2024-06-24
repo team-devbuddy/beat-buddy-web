be38c422a94c39eaf874927129fef1b37c260a9b
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-24 20:20:47 +0900

    feat: 메타데이터 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,12 @@ import { Inter } from 'next/font/google';
 import localFont from 'next/font/local';
 import './globals.css';
 import Header from '@/components/layout/Header';
-import Footer from '@/components/layout/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Netflix',
-  description: 'Netflix clone coding',
+  title: 'BeatBuddy',
+  description: 'beatbuddy',
   icons: {
     icon: '/logo.png',
   },
@@ -36,9 +35,7 @@ export default function RootLayout({
             <div className="flex h-screen w-full max-w-[600px] flex-col bg-BG-black">
               <Header />
               {/* Content */}
-              <div className="flex flex-1 items-center justify-center">
-                {children}
-              </div>
+              <div className="flex flex-1 items-center justify-center">{children}</div>
               {/* <Footer /> */}
             </div>
           </div>
