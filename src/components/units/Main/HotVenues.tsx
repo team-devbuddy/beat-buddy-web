7df606a6071e28e14bccb7ff7c531dabd6338d01
Shunamo <sueddi26@gmail.com>
2024-06-27 07:23:33 +0900

    feat : HotVenues.tsx

---
+++ src/components/units/Main/HotVenues.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Image from 'next/image';
+import { clubs } from '@/lib/data';
+
+export default function HotVenues() {
+  return (
+    <div className="flex flex-col mt-[1.5rem]">
+      <div className="flex flex-col items-start justify-between px-[1rem] py-[0.5rem]">
+        <span className="text-main2 font-Queensides text-lg">Hot</span>
+        <span className="text-gray200 text-body2">실시간으로 인기있는 베뉴 정보입니다.</span>
+      </div>
+      <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-x-[1.19rem] gap-y-[2.5rem] mx-[1rem] my-[1.5rem]">
+        {clubs.map((venue) => (
+          <div key={venue.id} className="relative flex flex-col">
+            <div className="relative h-[10.125rem] rounded-sm overflow-hidden flex-shrink-0">
+              <Image
+                src={venue.imageUrl}
+                alt={`${venue.name} image`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-sm"
+              />
+              <div className="absolute inset-0 club-gradient"></div>
+              <div className="absolute bottom-[0.62rem] right-[0.62rem] cursor-pointer">
+                <Image
+                  src="/icons/PinkHeart.svg"
+                  alt="pink-heart icon"
+                  width={32}
+                  height={32}
+                />
+              </div>
+            </div>
+            <div className="mt-[1rem]">
+              <h3 className="text-white font-bold text-body1 text-ellipsis">{venue.name}</h3>
+              <div className="flex flex-wrap gap-[0.5rem] mt-[0.75rem] mb-[1.06rem]">
+                <span className="bg-gray500 text-gray100 text-xs px-[0.38rem] py-[0.13rem] rounded">{venue.location}</span>
+                <span className="bg-gray500 text-gray100 text-xs px-[0.38rem] py-[0.13rem] rounded">{venue.genre1}</span>
+                <span className="bg-gray500 text-gray100 text-xs px-[0.38rem] py-[0.13rem] rounded">{venue.genre2}</span>
+              </div>
+              <div className="flex items-center space-x-[0.25rem] text-gray300">
+                <Image src="/icons/PinkHeart.svg" alt="pink-heart icon" width={20} height={16} />
+                <span>{venue.likes}</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
