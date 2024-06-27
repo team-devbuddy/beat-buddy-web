41efe21fac7737aa4cd2328ab1eb92315b7de892
Shunamo <sueddi26@gmail.com>
2024-06-28 03:02:43 +0900

    feat : detail 동적라우팅, dummy data 수정

--- src/components/units/BBPList/VenueCard.tsx
+++ src/components/units/BBPList/VenueCard.tsx
@@ -1,41 +1,49 @@
 import React from 'react';
 import Image from 'next/image';
-import { Club } from '@/lib/types';
+import Link from 'next/link';
+import { Club } from '@/lib/data';
 
 interface VenueCardProps {
   club: Club;
 }
 
-function VenueCard({ club }: VenueCardProps) {
+const VenueCard = ({ club }: VenueCardProps) => {
   return (
-    <div key={club.id} className="relative flex flex-col">
-      <div className="relative w-full" style={{ paddingBottom: '100%' }}>
-        <Image
-          src={club.imageUrl}
-          alt={`${club.name} image`}
-          layout="fill"
-          objectFit="cover"
-          className="rounded-md"
-        />
-        <div className="absolute inset-0 club-gradient"></div>
-        <div className="absolute top-[1.5rem] right-[1.5rem] cursor-pointer z-10">
-          <Image src="/icons/GrayHeart.svg" alt="gray-heart icon" width={32} height={32} />
+    <div className="relative flex flex-col">
+      <Link href={`/detail/${club.id}`}>
+        <div className="relative w-full" style={{ paddingBottom: '100%' }}>
+          <Image
+            src={club.imageUrl}
+            alt={`${club.name} image`}
+            layout="fill"
+            objectFit="cover"
+            className="rounded-sm"
+          />
+          <div className="absolute inset-0 club-gradient"></div>
+          <div className="absolute bottom-[0.62rem] right-[0.62rem] cursor-pointer">
+            <Image
+              src="/icons/PinkHeart.svg"
+              alt="pink-heart icon"
+              width={32}
+              height={32}
+            />
+          </div>
         </div>
-      </div>
+      </Link>
       <div className="mt-[1rem]">
-        <h3 className="text-title-20-bold">{club.name}</h3>
-        <div className="flex space-x-2 mt-[0.75rem] mb-[1.06rem]">
-          <span className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{club.location}</span>
-          <span className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{club.genre1}</span>
-          <span className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{club.genre2}</span>
+        <h3 className="text-white text-body1-16-bold text-ellipsis">{club.name}</h3>
+        <div className="flex flex-wrap gap-[0.5rem] mt-[0.75rem] mb-[1.06rem]">
+          {club.tags.map((tag, index) => (
+            <span key={index} className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{tag}</span>
+          ))}
         </div>
         <div className="flex items-center space-x-[0.25rem] text-gray300">
-          <Image src="/icons/PinkHeart.svg" alt="pink-heart icon" width={20} height={20} />
-          <span className="text-body3-12-medium ml-[0.25rem]">{club.likes}</span>
+          <Image src="/icons/PinkHeart.svg" alt="pink-heart icon" width={20} height={16} />
+          <span className='text-body3-12-medium'>{club.likes}</span>
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default VenueCard;
