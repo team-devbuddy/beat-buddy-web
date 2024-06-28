f48e0e483e8d55210c2df9853ae04f296d3ffa76
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-28 17:49:42 +0900

    fix: view fix 4

--- src/app/appLayout.tsx
+++ src/app/appLayout.tsx
@@ -1,19 +1,24 @@
 'use client';
 
-import setScreenHeight from '@/util/hooks';
 import React, { useEffect } from 'react';
 
 export default function ClientOnlyLayout({ children }: { children: React.ReactNode }) {
   useEffect(() => {
-    setScreenHeight();
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
 
-    // resize 이벤트가 발생하면 다시 계산하도록 아래 코드 추가
-    window.addEventListener('resize', setScreenHeight);
-    return () => window.removeEventListener('resize', setScreenHeight);
+    setVh();
+    window.addEventListener('resize', setVh);
+
+    return () => {
+      window.removeEventListener('resize', setVh);
+    };
   }, []);
 
   return (
-    <div className="h-fill-available min-h-real-screen flex h-screen w-full items-center justify-center">
+    <div className="min-h-webkit-fill-available flex h-screen w-full items-center justify-center">
       {/* 모바일 컨테이너 */}
       <div className="relative flex h-screen w-full max-w-[600px] flex-col bg-BG-black">
         {/* 콘텐츠 */}
