e4ff86fda9625d5f9cbb514f57064437c8ae6344
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-06-28 17:12:10 +0900

    fix: 모바일 뷰 버그 수정

--- src/app/appLayout.tsx
+++ src/app/appLayout.tsx
@@ -1,13 +1,16 @@
 'use client';
 
-import React from 'react';
-import { useVh } from '@/util/hooks';
+import React, { useEffect } from 'react';
 
 export default function ClientOnlyLayout({ children }: { children: React.ReactNode }) {
-  useVh();
+  let vh = 0;
+  useEffect(() => {
+    vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []);
 
   return (
-    <div className="flex h-full w-full items-center justify-center">
+    <div className="flex h-full w-full items-center justify-center" style={{ height: 'calc(var(--vh, 1vh) * 100)' }}>
       {/* 모바일 컨테이너 */}
       <div className="relative flex h-screen w-full max-w-[600px] flex-col bg-BG-black">
         {/* 콘텐츠 */}
--- src/util/hooks.ts
+++ src/util/hooks.ts
@@ -1,16 +1,3 @@
 import { useEffect } from 'react';
 
-export function useVh() {
-  useEffect(() => {
-    const setVh = () => {
-      document.documentElement.style.setProperty('--vh', `${window.innerHeight}px`);
-    };
-
-    window.addEventListener('resize', setVh);
-    setVh();
-
-    return () => {
-      window.removeEventListener('resize', setVh);
-    };
-  }, []);
-}
+export function useVh() {}
