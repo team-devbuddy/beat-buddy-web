02bf6e635af3df113597a56f2b43399b0adaabf7
김수현 <shunamo@gimsuhyeon-ui-MacBookPro.local>
2024-07-09 20:13:57 +0900

    feat : searchBar routing

--- src/components/units/Main/SearchBar.tsx
+++ src/components/units/Main/SearchBar.tsx
@@ -1,16 +1,44 @@
+'use client'
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
-export default function SearchBar() {
+export default function SearchBar({ isInputMode = false }) {
   return (
-    <div className="flex w-full bg-main items-center justify-between px-4 py-3">
-      <div className="relative w-full">
-        <input
-          className="w-full border-b-2 border-black bg-transparent px-2 py-2 text-BG-black placeholder:text-BG-black focus:outline-none"
-          placeholder="지금 가장 인기있는 클럽은?"
-        />
-        <Image src="/icons/search.svg" alt="search icon" width={24} height={24} className="absolute bottom-3 right-0" />
-      </div>
+    <div className="flex w-full items-center justify-between bg-main px-4 py-3">
+      {isInputMode ? (
+        <div className="relative w-full">
+          <input
+            className="w-full border-b-2 border-black bg-transparent px-2 py-2 text-BG-black placeholder:text-BG-black focus:outline-none"
+            placeholder="지금 가장 인기있는 클럽은?"
+          />
+          <Image
+            src="/icons/search.svg"
+            alt="search icon"
+            width={24}
+            height={24}
+            className="absolute bottom-3 right-0 cursor-pointer"
+            onClick={() => { alert('검색 기능 구현 필요'); }}
+          />
+        </div>
+      ) : (
+        <Link href="/search" className="block w-full">
+          <div className="relative w-full">
+            <input
+              className="w-full border-b-2 border-black bg-transparent px-2 py-2 text-BG-black placeholder:text-BG-black focus:outline-none"
+              placeholder="지금 가장 인기있는 클럽은?"
+              readOnly
+            />
+            <Image
+              src="/icons/search.svg"
+              alt="search icon"
+              width={24}
+              height={24}
+              className="absolute bottom-3 right-0"
+            />
+          </div>
+        </Link>
+      )}
     </div>
   );
 }
