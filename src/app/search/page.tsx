4ad33239a08eaefb7b83b9047b14e9a60ff8ead3
김수현 <shunamo@gimsuhyeon-ui-MacBookPro.local>
2024-07-09 21:27:57 +0900

    feat: genreSelector 컴포넌트 구현

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import MainHeader from '@/components/units/Main/MainHeader';
 import Footer from '@/components/units/Main/MainFooter';
 import SearchBar from '@/components/units/Main/SearchBar';
+import GenreSelector from '@/components/units/Search/GenreSelector';
 
 function SearchPage() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-BG-black text-white">
       <MainHeader />
       <SearchBar isInputMode />
-      <div className="grid grid-cols-1 gap-x-[1rem] gap-y-[2.5rem] md:grid-cols-2"></div>
+      <GenreSelector />
       <Footer />
     </div>
   );
