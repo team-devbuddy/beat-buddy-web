5bb8e87283ac6fd9c3c258dd062408f0d5132f51
김수현 <shunamo@gimsuhyeon-ui-MacBookPro.local>
2024-07-10 19:48:11 +0900

    feat : search edgecase 구현

--- src/components/units/Detail/Preview.tsx
+++ src/components/units/Detail/Preview.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React from 'react';
 import Image from 'next/image';
@@ -7,8 +7,8 @@ import { ClubProps } from '@/lib/types';
 
 const Preview = ({ club }: ClubProps) => {
   return (
-    <div className="relative flex h-[17.5rem] w-full flex-col justify-between">
-      <div className="z-10 flex items-start justify-between px-[1rem] py-[1rem]">
+    <div className="relative w-full h-[17.5rem] flex flex-col justify-between">
+      <div className="flex items-start justify-between px-[1rem] py-[1rem] z-10">
         <Link href="/" className="text-white">
           <Image src="/icons/ArrowLeft.svg" alt="back icon" width={24} height={24} />
         </Link>
@@ -23,14 +23,16 @@ const Preview = ({ club }: ClubProps) => {
           backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 37.5%, rgba(0, 0, 0, 0.72) 62.7%), url(${club.imageUrl})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-        }}></div>
-      <div className="z-20 flex flex-col items-start gap-[1rem] px-[1rem] py-[1.25rem] text-white">
+        }}
+      ></div>
+      <div className="flex flex-col items-start gap-[1rem] px-[1rem] py-[1.25rem] text-white z-20 ">
         <h1 className="text-title-24-bold">{club.name}</h1>
-        <div className="flex space-x-[0.5rem]">
+        <div className="flex space-x-[0.5rem] ">
           {club.tags.map((tag, index) => (
             <span
               key={index}
-              className="rounded-xs border border-gray500 bg-gray500 px-[0.38rem] py-[0.13rem] text-body3-12-medium text-gray100">
+              className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs"
+            >
               {tag}
             </span>
           ))}
