fbe0685e6d83116b3e54d89790bec98306a741ec
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-13 01:30:43 +0900

    feat: 카카오 로그인

--- src/components/units/Login/LoginModal.tsx
+++ src/components/units/Login/LoginModal.tsx
@@ -2,26 +2,20 @@ import { useEffect } from 'react';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+// window 객체의 타입을 확장하여 Kakao 객체를 선언합니다.
+declare global {
+  interface Window {
+    Kakao: any;
+  }
+}
 
 interface LoginModalProps {
   onClose: () => void;
 }
 
 const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
-  useEffect(() => {
-    const handleEscape = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
-
-    document.addEventListener('keydown', handleEscape);
-
-    return () => {
-      document.removeEventListener('keydown', handleEscape);
-    };
-  }, [onClose]);
-
   const handleBackgroundClick = (event: React.MouseEvent<HTMLDivElement>) => {
     if (event.currentTarget === event.target) {
       onClose();
@@ -43,15 +37,17 @@ const LoginModal: React.FC<LoginModalProps> = ({ onClose }) => {
         <div className="flex flex-col items-center justify-center">
           <h2 className="mb-3 text-center text-xl font-bold text-white">
             <p className="pb-1">버디님의 취향을</p>
-
             <p>저장할 계정이 필요해요</p>
           </h2>
           <p className="mb-10 text-center text-[0.9375rem] text-gray300">
             간편로그인하고 나만을 위한
             <br /> 베뉴를 추천 받아보세요
           </p>
-          <Link href="/onBoarding/agree">
-            <button className="flex items-center rounded-[0.38rem] bg-[#FEE500] py-[1.19rem] pl-[1.25rem] pr-20 text-black">
+
+          <Link href="https://beatbuddy.world/oauth2/authorization/kakao">
+            <button
+              // onClick={kakaoLoginHandler}
+              className="flex items-center rounded-[0.38rem] bg-[#FEE500] py-[1.19rem] pl-[1.25rem] pr-20 text-black">
               <Image src="/icons/kakao.svg" alt="kakao" width={22} height={22} className="mr-8" />
               카카오톡으로 계속하기
             </button>
---
+++ src/components/units/OnBoarding/onBoardingCustom.tsx
@@ -0,0 +1,37 @@
+'use client';
+import { useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { useRecoilState } from 'recoil';
+import { accessTokenState } from '@/context/recoil-context';
+
+export default function OnBoardingCustomPage() {
+  const searchParams = useSearchParams();
+  const [accessToken, setAccessToken] = useRecoilState(accessTokenState);
+
+  useEffect(() => {
+    const access = searchParams.get('access');
+    if (access) {
+      setAccessToken(access);
+    }
+  }, [searchParams]);
+
+  return (
+    <>
+      <div className="flex w-full flex-col px-4">
+        <h1 className="py-5 text-2xl font-bold leading-9 text-white">
+          수빈버디님의
+          <br />
+          취향 저격 베뉴를 추천받으세요
+        </h1>
+      </div>
+
+      <Link href="/onBoarding/myTaste/genre">
+        <button
+          className={`absolute bottom-0 flex w-full justify-center bg-[#EE1171] py-4 text-lg font-bold text-BG-black`}>
+          추천 받기
+        </button>
+      </Link>
+    </>
+  );
+}
