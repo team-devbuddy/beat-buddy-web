8678a13d579ac0fe4afe82278d43f02df6735f10
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-14 01:14:42 +0900

    feat: 닉네임 설정,중복확인

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,12 +1,43 @@
+const ACCESS =
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MiwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6IktBS0FPXzM2MTEzNjY5NjMiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcyMDg4NjU4NSwiZXhwIjoxNzIwODkzNzg1fQ.u-MqFUGzJElDrIbqA9x0ExsthHbXlqjeRPPm2o-pUg0';
+
 // 약관 동의
 export async function PostAgree(access: string, terms: { isLocationConsent: boolean; isMarketingConsent: boolean }) {
   const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/members/onboarding/consent`, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Access: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MSwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6IktBS0FPXzM2MTEzNjY5NjMiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcyMDg4MjUzOCwiZXhwIjoxNzIwODg5NzM4fQ.s471P0zSm44yLSueHq8EqSkfVIc3IOC69HI37oGHM7o`,
+      Access: ACCESS,
     },
     body: JSON.stringify(terms),
+  });
+
+  return response;
+}
+
+// 닉네임 중복확인
+export async function PostDuplicateCheck(access: string, nickname: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/members/onboarding/nickname/duplicate`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Access: ACCESS,
+    },
+    body: JSON.stringify({ nickname }),
+  });
+
+  return response;
+}
+
+// 닉네임 설정
+export async function PostNickname(access: string, nickname: string) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/members/onboarding/nickname`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Access: ACCESS,
+    },
+    body: JSON.stringify({ nickname }),
   });
 
   return response;
