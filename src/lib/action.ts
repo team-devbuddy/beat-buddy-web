bf66627e57f0ad3544bb4ec8c223d4373de753a2
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-14 16:26:48 +0900

    feat: 온보딩 - 분위기

--- src/lib/action.ts
+++ src/lib/action.ts
@@ -1,5 +1,5 @@
 const ACCESS =
-  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MiwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6IktBS0FPXzM2MTEzNjY5NjMiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcyMDg4ODQ2MiwiZXhwIjoxNzIwODk1NjYyfQ.0N_0CkZdwXEtohM7dZUK00fAv9jm-2yxCqXvESDRra0';
+  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJtZW1iZXJJZCI6MiwiY2F0ZWdvcnkiOiJhY2Nlc3MiLCJ1c2VybmFtZSI6IktBS0FPXzM2MTEzNjY5NjMiLCJyb2xlIjoiVVNFUiIsImlhdCI6MTcyMDk0MTM2MywiZXhwIjoxNzIwOTQ4NTYzfQ.Ol7XSIeVO_x8aFQfsRxssjjxRRUSTGpS2U0A3jdYFr8';
 
 // 약관 동의
 export async function PostAgree(access: string, terms: { isLocationConsent: boolean; isMarketingConsent: boolean }) {
@@ -52,6 +52,20 @@ export async function PostGenre(access: string, genres: { [key: string]: number 
       Access: ACCESS,
     },
     body: JSON.stringify(genres),
+  });
+
+  return response;
+}
+
+// 온보딩 - 선호 분위기
+export async function PostMood(access: string, moods: { [key: string]: number }): Promise<Response> {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SERVER_URL}/member-mood`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Access: ACCESS,
+    },
+    body: JSON.stringify(moods),
   });
 
   return response;
