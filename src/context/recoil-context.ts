688891fde211bc630e9594e21a53df23e763659b
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-15 13:03:44 +0900

    feat: 로그인 유지 플로우

--- src/context/recoil-context.ts
+++ src/context/recoil-context.ts
@@ -1,9 +1,17 @@
+// recoil-context.ts
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
+
 const { persistAtom } = recoilPersist();
 
 export const accessTokenState = atom<string | null>({
   key: 'accessTokenState',
   default: null,
+  effects_UNSTABLE: [persistAtom],
+});
+
+export const authState = atom<boolean>({
+  key: 'authState',
+  default: false,
   effects_UNSTABLE: [persistAtom],
 });
