50d0ba0576dcccc4b12b4c016d3cd8dcace8a94f
Suhyun Kim <sueddi26@gmail.com>
2024-07-19 03:44:39 +0900

    feat : bbp page 베뉴 연결.....

--- src/components/units/BBPList/VenueCard.tsx
+++ src/components/units/BBPList/VenueCard.tsx
@@ -1,45 +1,79 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { ClubProps } from '@/lib/types';
+import { useRecoilValue } from 'recoil';
+import { accessTokenState } from '@/context/recoil-context';
+import { handleHeartClick } from '@/lib/utils/heartbeatUtils';
+import { getBBP } from '@/lib/actions/recommend-controller/getBBP';
+import { BBPProps } from '@/lib/types';
 
+const VenueCard = () => {
+  const [clubs, setClubs] = useState<BBPProps[]>([]);
+  const [likedClubs, setLikedClubs] = useState<{ [key: number]: boolean }>({});
+  const accessToken = useRecoilValue(accessTokenState);
 
-const VenueCard = ({ club }: ClubProps) => {
+  useEffect(() => {
+    const fetchClubs = async (token: string) => {
+      try {
+        const data = await getBBP(token);
+        setClubs(data);
+      } catch (error) {
+        console.error('Error fetching clubs:', error);
+      }
+    };
+
+    if (accessToken) {
+      fetchClubs(accessToken);
+    }
+  }, [accessToken]);
+
+  const handleHeartClickWrapper = async (e: React.MouseEvent, venueId: number) => {
+    await handleHeartClick(e, venueId, likedClubs, setLikedClubs, accessToken);
+  };
+
   return (
-    <div className="relative flex flex-col">
-      <Link href={`/detail/${club.id}`}>
-        <div className="relative w-full" style={{ paddingBottom: '100%' }}>
-          <Image
-            src={club.imageUrl}
-            alt={`${club.name} image`}
-            layout="fill"
-            objectFit="cover"
-            className="rounded-sm"
-          />
-          <div className="absolute inset-0 club-gradient"></div>
-          <div className="absolute bottom-[0.62rem] right-[0.62rem] cursor-pointer">
-            <Image
-              src="/icons/PinkHeart.svg"
-              alt="pink-heart icon"
-              width={32}
-              height={32}
-            />
+    <>
+      {clubs.map((club) => (
+        <div key={club.venueId} className="relative mb-4 flex flex-col">
+          <Link href={`/detail/${club.venueId}`} passHref>
+            <div className="relative w-full pb-[100%]">
+              <Image
+                src={'/images/DefaultImage.png'} // 기본 이미지 URL 사용
+                alt={`${club.englishName} image`}
+                layout="fill"
+                objectFit="cover"
+                className="rounded-sm"
+              />
+              <div className="club-gradient absolute inset-0"></div>
+              <div
+                className="absolute bottom-[0.62rem] right-[0.62rem] cursor-pointer"
+                onClick={(e) => handleHeartClickWrapper(e, club.venueId)}>
+                <Image
+                  src={likedClubs[club.venueId] ? '/icons/FilledHeart.svg' : '/icons/PinkHeart.svg'}
+                  alt="pink-heart icon"
+                  width={32}
+                  height={32}
+                />
+              </div>
+            </div>
+          </Link>
+          <div className="mt-[1rem]">
+            <h3 className="text-ellipsis text-body1-16-bold text-white">{club.englishName}</h3>
+            <div className="mb-[1.06rem] mt-[0.75rem] flex flex-wrap gap-[0.5rem]">
+              {/* {club.tags.map((tag, index) => (
+                <span key={index} className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{tag}</span>
+              ))} */}
+            </div>
+            <div className="flex items-center space-x-[0.25rem] text-gray300">
+              <Image src="/icons/PinkHeart.svg" alt="pink-heart icon" width={20} height={16} />
+              {/* <span className='text-body3-12-medium'>{club.likes}</span> */}
+            </div>
           </div>
         </div>
-      </Link>
-      <div className="mt-[1rem]">
-        <h3 className="text-white text-body1-16-bold text-ellipsis">{club.name}</h3>
-        <div className="flex flex-wrap gap-[0.5rem] mt-[0.75rem] mb-[1.06rem]">
-          {club.tags.map((tag, index) => (
-            <span key={index} className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs">{tag}</span>
-          ))}
-        </div>
-        <div className="flex items-center space-x-[0.25rem] text-gray300">
-          <Image src="/icons/PinkHeart.svg" alt="pink-heart icon" width={20} height={16} />
-          <span className='text-body3-12-medium'>{club.likes}</span>
-        </div>
-      </div>
-    </div>
+      ))}
+    </>
   );
 };
 
--- src/components/units/Detail/Preview.tsx
+++ src/components/units/Detail/Preview.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 
 import React from 'react';
 import Image from 'next/image';
@@ -10,10 +10,10 @@ const Preview = ({ club }: ClubProps) => {
   const router = useRouter();
 
   return (
-    <div className="relative w-full h-[17.5rem] flex flex-col justify-between">
-      <div className="flex items-start justify-between px-[1rem] py-[1rem] z-10">
-      <button onClick={() => router.back()} aria-label="뒤로가기" className="text-white">
-      <Image src="/icons/ArrowLeft.svg" alt="back icon" width={24} height={24} />
+    <div className="relative flex h-[17.5rem] w-full flex-col justify-between">
+      <div className="z-10 flex items-start justify-between px-[1rem] py-[1rem]">
+        <button onClick={() => router.back()} aria-label="뒤로가기" className="text-white">
+          <Image src="/icons/ArrowLeft.svg" alt="back icon" width={24} height={24} />
         </button>
         <div className="flex items-center space-x-[1.25rem]">
           <Image src="/icons/share.svg" alt="share icon" width={32} height={32} />
@@ -26,16 +26,14 @@ const Preview = ({ club }: ClubProps) => {
           backgroundImage: `linear-gradient(180deg, rgba(0, 0, 0, 0.00) 37.5%, rgba(0, 0, 0, 0.72) 62.7%), url(${club.imageUrl})`,
           backgroundSize: 'cover',
           backgroundPosition: 'center',
-        }}
-      ></div>
-      <div className="flex flex-col items-start gap-[1rem] px-[1rem] py-[1.25rem] text-white z-20 ">
+        }}></div>
+      <div className="z-20 flex flex-col items-start gap-[1rem] px-[1rem] py-[1.25rem] text-white">
         <h1 className="text-title-24-bold">{club.name}</h1>
-        <div className="flex space-x-[0.5rem] ">
+        <div className="flex space-x-[0.5rem]">
           {club.tags.map((tag, index) => (
             <span
               key={index}
-              className="bg-gray500 text-gray100 text-body3-12-medium px-[0.38rem] py-[0.13rem] border border-gray500 rounded-xs"
-            >
+              className="rounded-xs border border-gray500 bg-gray500 px-[0.38rem] py-[0.13rem] text-body3-12-medium text-gray100">
               {tag}
             </span>
           ))}
