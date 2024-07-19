7916fce4b2e181e951485d22a6ae3239cf09b466
Suhyun Kim <sueddi26@gmail.com>
2024-07-19 19:57:54 +0900

    feat : heartbeat 컴포넌트

--- src/components/units/Main/Hot-Chart.tsx
+++ src/components/units/Main/Hot-Chart.tsx
@@ -37,7 +37,7 @@ function HotVenues({ title = 'Hot', description = '실시간으로 인기있는 
   return (
     <div className="mt-[1.5rem] flex flex-col">
       <div className="flex flex-col items-start justify-between px-[1rem] py-[0.5rem]">
-        <span className="font-queensides text-lg text-main2">{title}</span>
+        <span className="font-queensides text-main-queen text-main2">{title}</span>
         <span className="text-body2-15-medium text-gray200">{description}</span>
       </div>
       <ClubsList clubs={clubs} />
