476df0a2763ce4d3891820bca55b40d9148566f4
Suhyun Kim <sueddi26@gmail.com>
2024-07-20 04:34:02 +0900

    feat : 하트비트 모달.. 지도버튼..ㅋ (하트비트로직은수정필요ㅠㅠ)

--- src/components/units/Search/SearchResults.tsx
+++ src/components/units/Search/SearchResults.tsx
@@ -4,6 +4,7 @@ import ClubList from '../Main/ClubList';
 import SearchHeader from './SearchHeader';
 import { SearchResultsProps } from '@/lib/types';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function SearchResults({ filteredClubs }: SearchResultsProps) {
   const [searchQuery, setSearchQuery] = useState('');
@@ -13,18 +14,24 @@ export default function SearchResults({ filteredClubs }: SearchResultsProps) {
   }, [filteredClubs]);
 
   return (
-    <div className="flex w-full flex-col">
+    <div className="relative flex w-full flex-col">
       <SearchHeader searchQuery={searchQuery} setSearchQuery={setSearchQuery} />
       <div className="flex flex-col bg-BG-black">
         {filteredClubs.length > 0 ? (
           <ClubList clubs={filteredClubs} />
         ) : (
-          <div className="flex flex-col h-full items-center justify-center py-[10rem]">
-            <Image src="/icons/caution.svg" alt='caution image' width={56.679} height={52}/>
-            <p className="text-gray300 mt-[1.25rem]">검색 결과가 없습니다.</p>
+          <div className="flex h-full flex-col items-center justify-center py-[10rem]">
+            <Image src="/icons/caution.svg" alt="caution image" width={56.679} height={52} />
+            <p className="mt-[1.25rem] text-gray300">검색 결과가 없습니다.</p>
           </div>
         )}
       </div>
+      <Link href="/map" passHref>
+        <div className="absolute bottom-8 left-1/2 z-50 flex -translate-x-1/2 transform cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
+          <Image src="/icons/map.svg" alt="Map Icon" width={16} height={15.08} />
+          <span className="ml-[0.5rem]">지도 보기</span>
+        </div>
+      </Link>
     </div>
   );
 }
