e8f60072cf889b45b1450cde3b7fb3626d362a4c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:35:03 +0900

    feat: 랜딩5

--- src/components/units/home/CustomerMain.tsx
+++ src/components/units/home/CustomerMain.tsx
@@ -2,6 +2,7 @@ import Landing1 from './Landing1';
 import Landing2 from './Landing2';
 import Landing3 from './Landing3';
 import Landing4 from './Landing4';
+import Landing5 from './Landing5';
 
 export default function CustomerMain() {
   return (
@@ -10,6 +11,7 @@ export default function CustomerMain() {
       <Landing2 />
       <Landing3 />
       <Landing4 />
+      <Landing5 />
     </div>
   );
 }
