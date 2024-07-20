934bc0191e507988dcd8c7be8d701fd2efa43000
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:36:34 +0900

    feat: 랜딩 마무리

--- src/components/units/home/Landing5.tsx
+++ src/components/units/home/Landing5.tsx
@@ -6,10 +6,10 @@ export default function Landing5() {
       className="flex h-screen w-full flex-col items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/images/onBoarding/Landing5.png')" }}>
       <div className="mb-[1.25rem] flex w-full justify-center">
-        <Image src={'/images/onBoarding/Landing1.png'} alt="colored logo" width={221} height={125} />
+        <Image src={'/images/onBoarding/logo.svg'} alt="colored logo" width={63} height={60} />
       </div>
-      <span className="mb-[1.25rem] text-body1-16-bold text-white">지금 나에게 맞는 베뉴를 추천 받고 싶나요?</span>
-      <div className="flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
+      <span className="mb-[1.25rem] text-body1-16-bold text-white">Feel the beat, Live the night</span>
+      <div className="mt-[3.75rem] flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
         <span className="text-body2-15-bold">비트버디 시작하기</span>
       </div>
     </div>
