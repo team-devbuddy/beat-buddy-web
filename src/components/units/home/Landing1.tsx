d95ed015ebbf3cc3789230700c1877c7f51e3cbd
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:57:16 +0900

    feat: 랜딩페이지 로그인 모달 연결

--- src/components/units/home/Landing1.tsx
+++ src/components/units/home/Landing1.tsx
@@ -1,6 +1,19 @@
+'use client';
+import { AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
+import { useState } from 'react';
+import LoginModal from '../Login/LoginModal';
 
 export default function Landing1() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleLoginClick = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
   return (
     <div
       className="flex h-screen w-full flex-col items-center justify-center bg-cover bg-center"
@@ -10,8 +23,18 @@ export default function Landing1() {
       </div>
       <span className="mb-[1.25rem] text-body1-16-bold text-white">지금 나에게 맞는 베뉴를 추천 받고 싶나요?</span>
       <div className="flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
-        <span className="text-body2-15-bold">비트버디 시작하기</span>
+        <span className="text-body2-15-bold" onClick={handleLoginClick}>
+          비트버디 시작하기
+        </span>
       </div>
+
+      <AnimatePresence>
+        {isModalOpen && (
+          <div className="inset-0 flex items-center justify-center">
+            <LoginModal onClose={handleCloseModal} />
+          </div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
