426f17ce32ef61cb1e077afa2f3dd542683674c1
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:29:08 +0900

    feat: 랜딩1

--- src/components/units/home/CustomerMain.tsx
+++ src/components/units/home/CustomerMain.tsx
@@ -1,9 +1,11 @@
+import Landing1 from './Landing1';
 import Landing3 from './Landing3';
 import Landing4 from './Landing4';
 
 export default function CustomerMain() {
   return (
     <div className="flex h-full w-full flex-col">
+      <Landing1 />
       <Landing3 />
       <Landing4 />
     </div>
---
+++ src/components/units/home/Landing1.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image';
+
+export default function Landing1() {
+  return (
+    <div
+      className="flex h-screen w-full flex-col items-center justify-center bg-cover bg-center"
+      style={{ backgroundImage: "url('/images/onBoarding/Landing1-bg.png')" }}>
+      <div className="mb-[1.25rem] flex w-full justify-center">
+        <Image src={'/images/onBoarding/Landing1.png'} alt="colored logo" width={221} height={125} />
+      </div>
+      <span className="mb-[1.25rem] text-body1-16-bold text-white">지금 나에게 맞는 베뉴를 추천 받고 싶나요?</span>
+      <div className="flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
+        <span className="text-body2-15-bold">비트버디 시작하기</span>
+      </div>
+    </div>
+  );
+}
