07ec782b19b0495a9f6fd87867ec33437352493a
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 11:06:55 +0900

    style: 온보딩 스타일 수정

--- src/app/onBoarding/custom/page.tsx
+++ src/app/onBoarding/custom/page.tsx
@@ -1,9 +1,11 @@
+import Prev from '@/components/common/Prev';
 import OnBoardingCustom from '@/components/units/OnBoarding/onBoardingCustom';
 
 export default function onBoardingCustomPage() {
   return (
-    <>
+    <div className="flex flex-col">
+      <Prev url={'/onBoarding/name'} />
       <OnBoardingCustom />
-    </>
+    </div>
   );
 }
--- src/app/onBoarding/name/page.tsx
+++ src/app/onBoarding/name/page.tsx
@@ -1,8 +1,10 @@
+import Prev from '@/components/common/Prev';
 import OnboardingName from '@/components/units/OnBoarding/OnboardingName';
 
 export default function OnBoardingNamePage() {
   return (
     <div className="w-full">
+      <Prev url={'/onBoarding'} />
       <OnboardingName />
     </div>
   );
