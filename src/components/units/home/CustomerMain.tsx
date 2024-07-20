3d653dcd3292f634b28e906a15e9507e863d9d59
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:18:15 +0900

    feat: 랜딩 3

--- src/components/units/home/CustomerMain.tsx
+++ src/components/units/home/CustomerMain.tsx
@@ -1,7 +1,9 @@
+import Landing3 from './Landing3';
+
 export default function CustomerMain() {
   return (
-    <div>
-      <>123</>
+    <div className="flex h-full w-full flex-col">
+      <Landing3 />
     </div>
   );
 }
