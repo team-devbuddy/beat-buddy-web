ecb36c3acddf9436f2d3fb3673580b386f5185a0
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-20 16:25:28 +0900

    feat: Landing4

--- src/components/units/home/CustomerMain.tsx
+++ src/components/units/home/CustomerMain.tsx
@@ -1,9 +1,11 @@
 import Landing3 from './Landing3';
+import Landing4 from './Landing4';
 
 export default function CustomerMain() {
   return (
     <div className="flex h-full w-full flex-col">
       <Landing3 />
+      <Landing4 />
     </div>
   );
 }
