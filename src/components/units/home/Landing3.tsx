993dc566047f3d4ed8153c9185171e902d99821c
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-07-24 19:25:09 +0900

    feat: 온보딩 애니메이션 적용

--- src/components/units/home/Landing3.tsx
+++ src/components/units/home/Landing3.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import { motion } from 'framer-motion';
+import { clubEffect } from '@/lib/animation';
 
 export default function Landing3() {
   return (
@@ -6,25 +8,37 @@ export default function Landing3() {
       style={{ backgroundImage: `url('/images/onBoarding/Landing3-bg.png')` }}
       className="flex h-screen min-h-screen w-full snap-mandatory snap-start snap-always flex-col justify-center bg-main px-6 py-[2.5rem]">
       <div className="flex flex-col">
-        <div className="flex flex-col items-end gap-1">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={clubEffect}
+          className="flex flex-col items-end gap-1">
           <p className="text-white">02. 맞춤형 추천</p>
           <p className="font-bold text-black">익숙한 베뉴에서 벗어나</p>
           <p className="font-bold text-black">새로운 베뉴에 가보고 싶으신가요?</p>
-        </div>
+        </motion.div>
 
-        <div className="flex w-full justify-center">
+        <motion.div initial="hidden" whileInView="visible" variants={clubEffect} className="flex w-full justify-center">
           <Image src="/images/onBoarding/Landing3-1.png" width={327} height={277} alt="123" className="mt-10" />
-        </div>
+        </motion.div>
 
-        <div className="mt-10 flex w-full justify-center bg-black px-5 py-[0.38rem] text-white">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={clubEffect}
+          className="mt-10 flex w-full justify-center bg-black px-5 py-[0.38rem] text-white">
           버디님 취향에 맞는 베뉴를 찾아드릴게요!
-        </div>
+        </motion.div>
 
-        <div className="leading-12 mt-5 text-center text-xs">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={clubEffect}
+          className="leading-12 mt-5 text-center text-xs">
           비트버디만의 온보딩 테스트를 이용해 버디님이 좋아하시는
           <br />
           음악, 분위기를 가진 베뉴를 추천해드려요
-        </div>
+        </motion.div>
       </div>
     </div>
   );
--- src/components/units/home/Landing5.tsx
+++ src/components/units/home/Landing5.tsx
@@ -3,6 +3,9 @@ import { AnimatePresence } from 'framer-motion';
 import Image from 'next/image';
 import { useState } from 'react';
 import LoginModal from '../Login/LoginModal';
+import { clubEffect } from '@/lib/animation';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
 
 export default function Landing5() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -18,15 +21,31 @@ export default function Landing5() {
     <div
       className="flex h-screen min-h-screen w-full snap-mandatory snap-start snap-always flex-col items-center justify-center bg-cover bg-center"
       style={{ backgroundImage: "url('/images/onBoarding/Landing5.png')" }}>
-      <div className="mb-[1.25rem] flex w-full justify-center">
+      <motion.div
+        initial="hidden"
+        whileInView="visible"
+        variants={clubEffect}
+        className="mb-[1.25rem] flex w-full justify-center">
         <Image src={'/images/onBoarding/logo.svg'} alt="colored logo" width={63} height={60} />
-      </div>
-      <span className="mb-[1.25rem] text-body1-16-bold text-white">Feel the beat, Live the night</span>
-      <div
-        onClick={handleLoginClick}
-        className="mt-[3.75rem] flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
-        <span className="text-body2-15-bold">비트버디 시작하기</span>
-      </div>
+      </motion.div>
+      <motion.span
+        initial="hidden"
+        whileInView="visible"
+        variants={clubEffect}
+        className="mb-[1.25rem] text-body1-16-bold text-white">
+        Feel the beat, Live the night
+      </motion.span>
+
+      <Link href="#1">
+        <motion.div
+          initial="hidden"
+          whileInView="visible"
+          variants={clubEffect}
+          // onClick={handleLoginClick}
+          className="mt-[3.75rem] flex cursor-pointer items-center justify-center rounded-full bg-main px-[1.25rem] py-[0.75rem] text-body3-12-bold text-BG-black">
+          <span className="text-body2-15-bold">비트버디 시작하기</span>
+        </motion.div>
+      </Link>
 
       <AnimatePresence>
         {isModalOpen && (
