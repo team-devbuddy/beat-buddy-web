d2384236cbd178ec8d240228318b51f7f8129086
Suhyun Kim <sueddi26@gmail.com>
2024-07-28 22:30:01 +0900

    feat : bbp filter button ui

--- src/components/units/BBPList/BBPMain.tsx
+++ src/components/units/BBPList/BBPMain.tsx
@@ -18,6 +18,8 @@ import BBPListSkeleton from '@/components/common/skeleton/BBPListSkeleton';
 
 
 
+
+
 const BBPickHeader = dynamic(() => import('./BBPHeader'), { ssr: false });
 
 export default function BBPMain() {
