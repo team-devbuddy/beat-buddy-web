42a5c62b91203362a39070cd564c33d2047c990e
Suhyun Kim <sueddi26@gmail.com>
2024-07-30 23:21:12 +0900

    feat : search, dropdown filtering 분리

--- src/components/units/Main/ClubList.tsx
+++ src/components/units/Main/ClubList.tsx
@@ -56,7 +56,7 @@ const getFilteredTags = (tags: string[]) => {
 
 export default function ClubList({ clubs, likedClubs, heartbeatNums, handleHeartClickWrapper }: ClubsListProps) {
   return (
-    <div className="flex w-full flex-col">
+    <div className="flex w-full bg-[#131415] border-[#131415] flex-col">
       <div className="mx-[1rem] my-[1.5rem] grid grid-cols-2 gap-x-[1rem] gap-y-[2.5rem] sm:grid-cols-2 md:grid-cols-3">
         {clubs.map((venue) => {
           const firstImageUrl =
