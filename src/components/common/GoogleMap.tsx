2afc3b622b8582c51c9d55ff014cb816628c9073
Suhyun Kim <sueddi26@gmail.com>
2024-07-30 17:35:00 +0900

    feat : 구글맵 클러스터링 스타일링

--- src/components/common/GoogleMap.tsx
+++ src/components/common/GoogleMap.tsx
@@ -206,7 +206,6 @@ const GoogleMap = forwardRef<{ filterAddressesInView: () => void }, GoogleMapPro
           // 초기화
           markerCluster?.clearMarkers();
           markers.forEach((marker) => marker.setMap(null));
-          markerCluster?.clearMarkers();
           setMarkers([]);
 
           const newMarkers: google.maps.Marker[] = [];
