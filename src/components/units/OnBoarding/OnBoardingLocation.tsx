9fcea2a223192eebdbe2d8a048f8d899e97a642f
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-02 00:31:32 +0900

    fix: 이미지 안뜨는거 성공

--- src/components/units/OnBoarding/OnBoardingLocation.tsx
+++ src/components/units/OnBoarding/OnBoardingLocation.tsx
@@ -15,11 +15,11 @@ const locationMap: { [key: string]: string } = {
 };
 
 const locationImages: { [key: string]: string } = {
-  홍대: '/images/onboarding/background/onboarding-4.webp',
-  압구정: '/images/onboarding/background/onboarding-2.webp',
-  이태원: '/images/onboarding/background/onboarding-5.webp',
-  '강남/신사': '/images/onboarding/background/onboarding-7.webp',
-  기타: '/images/onboarding/background/onboarding-9.webp',
+  홍대: '/images/onBoarding/background/onboarding-4.webp',
+  압구정: '/images/onBoarding/background/onboarding-2.webp',
+  이태원: '/images/onBoarding/background/onboarding-5.webp',
+  '강남/신사': '/images/onBoarding/background/onboarding-7.webp',
+  기타: '/images/onBoarding/background/onboarding-9.webp',
 };
 
 const locations = Object.keys(locationMap);
