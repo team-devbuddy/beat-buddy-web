d7a4b9c357fca7e0e647d125f871fee88ae929cf
김동혁 <132553964+ddhelop@users.noreply.github.com>
2024-08-02 12:52:59 +0900

    style: 제정 -> 페업 텍스트 변경

--- src/components/units/Detail/CustomerService.tsx
+++ src/components/units/Detail/CustomerService.tsx
@@ -8,11 +8,11 @@ const CustomerService = () => {
   return (
     <Link
       href="https://forms.gle/rcSfxUegbNykLnZD7"
-      className="flex cursor-pointer items-center justify-between border-t border-gray500 bg-BG-black px-[1rem] pt-[1.25rem] pb-[3.75rem] text-white">
+      className="flex cursor-pointer items-center justify-between border-t border-gray500 bg-BG-black px-[1rem] pb-[3.75rem] pt-[1.25rem] text-white">
       <div>
         <span className="block text-body2-15-bold">잘못된 정보가 있나요?</span>
         <span className="mt-1 block text-body3-12-medium text-gray300">
-          수정이 필요하거나 제정된 항목이라면 알려주세요!
+          수정이 필요하거나 폐업한 업장이라면 알려주세요!
         </span>
       </div>
       <Image src="/icons/ArrowHeadRight.svg" alt="Arrow Head right icon" width={24} height={24} />
