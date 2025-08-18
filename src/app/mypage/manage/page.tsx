2ec355cb926a54a3bc684140745f93e227271103
Suhyun Kim <sueddi26@gmail.com>
2025-08-18 18:43:24 +0900

    fix : mypage management

--- src/app/mypage/manage/page.tsx
+++ src/app/mypage/manage/page.tsx
@@ -1,4 +1,3 @@
-
 import MyPageManage from '@/components/units/Mypage/MyPageManage';
 import Link from 'next/link';
 import Image from 'next/image';
@@ -6,13 +5,13 @@ import MyEventManagement from '@/components/units/MyEvent/MyEventManagement';
 export default function MyPageManagePage() {
   return (
     <div className="flex w-full flex-col">
-      <div className="flex items-center px-5 pt-[0.62rem] pb-[0.88rem]">
+      <div className="flex items-center px-5 pb-[0.88rem] pt-[0.62rem]">
         <Link href={'/mypage'}>
           <Image src="/icons/arrow_back_ios.svg" alt="back" width={24} height={24} />
         </Link>
         <span className="text-subtitle-20-bold text-white">내 프로필 관리</span>
       </div>
-      <MyEventManagement />
+      <MyPageManage />
     </div>
   );
 }
